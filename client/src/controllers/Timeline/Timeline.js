0ef359d1fc3475b09271e5650b4b785f37e491bf
intrvertmichael <michaelpaguay@gmail.com>
2020-11-24 15:07:56 -0500

    timeline branch and controller created

---
+++ client/src/controllers/Timeline/Timeline.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Timeline.scss'
+
+const Timeline = props => {
+    return (
+        <div>
+            This is the timeline
+        </div>
+    )
+}
+
+export default Timeline
