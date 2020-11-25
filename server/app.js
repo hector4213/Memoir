90d9daae70b1b1dc57586397de81aedaa5b95f95
hector4213 <hectorpwns@gmail.com>
2020-11-25 01:30:52 -0500

    add to app

--- server/app.js
+++ server/app.js
@@ -1,14 +1,20 @@
 const express = require('express')
+const middleware = require('./utils/middleware')
 const logger = require('morgan')
+const cors = require('cors')
 
 const api = require('./api')
 
 const app = express()
 
 app.use(logger('tiny'))
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 app.use('/api', api)
+app.use(middleware.errorHandler)
+app.use(middleware.tokenExtractor)
+app.use(middleware.unknownEndpoint)
 
 module.exports = app
