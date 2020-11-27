955f38bd1eba0a344b0ac7f4cf38d09fe158a797
hector4213 <hectorpwns@gmail.com>
2020-11-27 15:16:04 -0500

    add relationship

--- server/api/users/users.model.js
+++ server/api/users/users.model.js
@@ -5,6 +5,20 @@ class User extends Model {
   static get tableName() {
     return tableNames.users
   }
+
+  static get relationMappings() {
+    const Story = require('../story/story.model')
+    return {
+      story: {
+        relation: Model.HasManyRelation,
+        modelClass: Story,
+        join: {
+          from: 'users.id',
+          to: 'story.id',
+        },
+      },
+    }
+  }
 }
 
 module.exports = User
