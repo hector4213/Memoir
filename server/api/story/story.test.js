c95998c787054ecb7138a39f1f0a3d16701dfc08
hector4213 <hectorpwns@gmail.com>
2020-11-27 14:36:25 -0500

    start with test

---
+++ server/api/story/story.test.js
@@ -0,0 +1,25 @@
+const { expect } = require('chai')
+const supertest = require('supertest')
+const { knex } = require('../../db/config')
+const app = require('../../app')
+
+describe('POST /story/create', () => {
+  beforeEach(async () => {
+    await knex.migrate.rollback()
+    await knex.migrate.latest()
+    await knex.seed.run()
+  })
+
+  afterEach(async () => {
+    await knex.migrate.rollback()
+  })
+
+  it('User can successfull create a new story', async () => {
+    const response = supertest(app)
+      .get('/api/story/create')
+      .expect('Content-type', /json/)
+      .expect(201)
+
+    expect(response.body).to.not.be.undefined()
+  })
+})
