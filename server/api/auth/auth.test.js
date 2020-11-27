9ced605cccc28d8fe228a7a480ae5d62abc35c55
hector4213 <hectorpwns@gmail.com>
2020-11-27 13:07:44 -0500

    auth test

--- server/api/auth/auth.test.js
+++ server/api/auth/auth.test.js
@@ -1,12 +1,36 @@
+process.env.NODE_ENV = 'test'
 const { expect } = require('chai')
 const supertest = require('supertest')
 const app = require('../../app')
+const { knex } = require('../../db/config')
+const User = require('../users/users.model')
 
-describe('POST for /auth/signup', () => {
-  it('Creates a new user', () => {
-    const response = supertest(app)
-      .post('/api/auth/signup')
-      .send({ username: 'testy', email: 'testy@test.com' })
-      .expect(201)
+describe('POST for /auth', () => {
+  beforeEach(async () => {
+    await knex.migrate.rollback()
+    await knex.migrate.latest()
+    await knex.seed.run()
+  })
+
+  afterEach(async () => {
+    await knex.migrate.rollback()
+  })
+
+  it('Creates a new user /signup', async () => {
+    const newUser = {
+      username: 'testy',
+      email: 'testy@testy.com',
+      password: 'Hello!123',
+    }
+    const response = await supertest(app).post('/api/auth/signup').send(newUser)
+    expect(response.status).to.eql(201)
+    expect(response.body).to.include.keys('user', 'token')
+  })
+
+  it('test if user can log in /login', async () => {
+    const userLogin = {
+      username: 'testthedb',
+      password: 'React!123',
+    }
   })
 })
