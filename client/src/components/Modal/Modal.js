7cc337f75e5769d68c30d891fa223db14760d6a8
intrvertmichael <michaelpaguay@gmail.com>
2020-11-27 17:07:14 -0500

    added input fields for login and register

--- client/src/components/Modal/Modal.js
+++ client/src/components/Modal/Modal.js
@@ -1,13 +1,31 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './Modal.scss'
 
+import RegisterForm from './RegisterForm'
+import LogInForm from './LogInForm'
+
 const Modal = props => {
+    const [registerTab, isRegisterTab] = useState(true)
     const {showModal} = props
 
     return (
         <div className='modal' onClick={()=>showModal(false)}>
             <div className='modal-content' onClick={e => e.stopPropagation()}>
-                This is a modal
+                <div className='tabs'>
+                    <button
+                        className={registerTab? 'active' : '' }
+                        onClick={()=>isRegisterTab(true)}
+                    >
+                    Register </button>
+
+                    <button
+                        className={registerTab? '' : 'active' }
+                        onClick={()=>isRegisterTab(false)}
+                    >
+                    Log In </button>
+                </div>
+
+                {registerTab? <RegisterForm /> : <LogInForm />}
             </div>
         </div>
     )
---
+++ client/src/components/Modal/RegisterForm.js
@@ -0,0 +1,41 @@
+import React, {useState} from 'react'
+import Button from '../Button/Button'
+// label, onClick, transparent, extraClass
+
+const RegisterForm = props => {
+    const [formInfo, setFormInfo] = useState()
+
+    return (
+        <form>
+
+            <input type='text' placeholder='Enter your Username'
+                onChange={ e =>{
+                setFormInfo( {...formInfo, username : e.target.value })
+                }}
+            />
+
+            <input type='password' placeholder='Enter your Password'
+                onChange={ e =>{
+                    setFormInfo( {...formInfo, password : e.target.value })
+                }}
+            />
+
+            <input type='password' placeholder='Re-enter your Password'
+                onChange={ e =>{
+                    setFormInfo( {...formInfo, confirmPassword : e.target.value })
+                }}
+            />
+
+            <Button {...{
+                label:'Submit',
+                transparent: false,
+                onClick: e => {
+                    e.preventDefault()
+                    console.log(formInfo)
+                }
+            }}/>
+        </form>
+    )
+}
+
+export default RegisterForm
