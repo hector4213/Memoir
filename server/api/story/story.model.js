b3251506fe3283031ed8758c582c03911bc392c6
hector4213 <hectorpwns@gmail.com>
2020-11-27 14:38:01 -0500

    add route and start model

---
+++ server/api/story/story.model.js
@@ -0,0 +1,22 @@
+const { Model } = require('../../db/config')
+const tableNames = require('../../constants/tableNames')
+
+class Story extends Model {
+  static get tableNames() {
+    return tableNames.story
+  }
+
+  static get relationMappings() {
+    const User = require('../users/users.model')
+    return {
+      id: {
+        relation: Model.BelongsToOneRelation,
+        modelClass: User,
+        join: {
+          from: 'story.id',
+          to: 'users.id',
+        },
+      },
+    }
+  }
+}
