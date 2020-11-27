8328b159b0bcd457fdecd7ea9ae7a0ae7f43c47d
hector4213 <hectorpwns@gmail.com>
2020-11-27 15:16:29 -0500

    add story table

--- server/db/migrations/20201127144225_ADD_STORY_TABLE.js
+++ server/db/migrations/20201127144225_ADD_STORY_TABLE.js
@@ -1,8 +1,19 @@
+const tableNames = require('../../constants/tableNames')
+exports.up = async (knex) => {
+  await knex.schema.createTable(tableNames.story, (table) => {
+    table.increments().primary()
+    table.string('title').notNullable()
+    table.datetime('created').defaultTo(knex.fn.now())
+  })
+}
 
-exports.up = function(knex) {
-  
-};
-
-exports.down = function(knex) {
-  
-};
+exports.down = async (knex) => {
+  await Promise.all(
+    [
+      tableNames.users,
+      tableNames.story,
+      tableNames.entry,
+      tableNames.format,
+    ].map((tableName) => knex.schema.dropTableIfExists(tableName))
+  )
+}
