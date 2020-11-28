6c94658f6db1b04c615db4721961f19cc5322bde
hector4213 <hectorpwns@gmail.com>
2020-11-27 21:13:23 -0500

    add FK referenceing to story table

--- server/db/migrations/20201127144225_ADD_STORY_TABLE.js
+++ server/db/migrations/20201127144225_ADD_STORY_TABLE.js
@@ -2,6 +2,11 @@ const tableNames = require('../../constants/tableNames')
 exports.up = async (knex) => {
   await knex.schema.createTable(tableNames.story, (table) => {
     table.increments().primary()
+    table
+      .integer('author_id')
+      .references('id')
+      .inTable(tableNames.users)
+      .notNullable()
     table.string('title').notNullable()
     table.datetime('created').defaultTo(knex.fn.now())
   })
