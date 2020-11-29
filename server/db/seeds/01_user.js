b8e33c22b0b80430edc312182409537f7507c2da
hector4213 <hectorpwns@gmail.com>
2020-11-29 11:55:05 -0500

    add constant tablename

--- server/db/seeds/01_user.js
+++ server/db/seeds/01_user.js
@@ -1,7 +1,8 @@
 const users = require('./seedData/users')
+const tableNames = require('../../constants/tableNames')
 
 exports.seed = function (knex, Promise) {
-  return knex('users')
+  return knex(tableNames.users)
     .del()
     .then(() => {
       return knex('users').insert(users)
