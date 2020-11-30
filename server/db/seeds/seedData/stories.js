9da42ea00bd6ae73f158ff0415bd3d0ddbdb58a2
hector4213 <hectorpwns@gmail.com>
2020-11-30 00:09:17 -0500

    add seeds

---
+++ server/db/seeds/seedData/stories.js
@@ -0,0 +1,62 @@
+module.exports = [
+  {
+    id: 0,
+    title: 'my first story',
+    author_id: 1,
+    created: '2018-11-29T00:00:00.000Z',
+  },
+  {
+    id: 1,
+    title: 'my new car',
+    author_id: 2,
+    created: '2020-12-29T00:00:00.000Z',
+  },
+  {
+    id: 2,
+    title: 'Got married',
+    author_id: 3,
+    created: '2020-05-29T00:00:00.000Z',
+  },
+  {
+    id: 3,
+    title: 'first tech job!',
+    author_id: 3,
+    created: '2019-01-29T00:00:00.000Z',
+  },
+  {
+    id: 4,
+    title: 'Life and times of myself',
+    author_id: 4,
+    created: '2020-02-29T00:00:00.000Z',
+  },
+  {
+    id: 5,
+    title: 'Just wanna write about my sadness',
+    author_id: 5,
+    created: '2020-11-29T00:00:00.000Z',
+  },
+  {
+    id: 6,
+    title: 'I just won the lottery',
+    author_id: 6,
+    created: '2019-10-29T00:00:00.000Z',
+  },
+  {
+    id: 7,
+    title: 'Jesus bejesus, i got baptized!',
+    author_id: 7,
+    created: '2018-09-29T00:00:00.000Z',
+  },
+  {
+    id: 8,
+    title: 'Cults are cool and all check out my cult stuff',
+    author_id: 8,
+    created: '2018-08-29T00:00:00.000Z',
+  },
+  {
+    id: 9,
+    title: 'Got litty and had a good night!',
+    author_id: 9,
+    created: '2018-10-29T00:00:00.000Z',
+  },
+]
