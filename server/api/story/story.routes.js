01dcacbe32880a895d278a23d7bcf47e8f7f5f45
hector4213 <hectorpwns@gmail.com>
2020-11-30 00:14:33 -0500

    can get all stories and single story by id

--- server/api/story/story.routes.js
+++ server/api/story/story.routes.js
@@ -1,34 +1,76 @@
 const express = require('express')
 const jwt = require('../../lib/jwt')
 const Story = require('./story.model')
+const User = require('../users/users.model')
 const yup = require('yup')
 
 const router = express.Router()
 
-const schema = yup.object.shape({
+const schema = yup.object().shape({
   title: yup.string().trim().min(3).required(),
 })
 
+// GET ALL USER STORIES
+
 router.get('/', async (req, res, next) => {
-  const stories = await Story.query()
-  res.status(200).json(stories)
+  try {
+    const allStories = await Story.query()
+      .withGraphFetched('entries')
+      .withGraphFetched('user(userInfo)')
+      .modifiers({
+        userInfo(builder) {
+          builder.select('id', 'username')
+        },
+      })
+    res.status(200).json(allStories)
+  } catch (error) {
+    next(error)
+  }
 })
+
+//GET all single story
+
+router.get('/:id', async (req, res, next) => {
+  const { id } = req.params
+  try {
+    const story = await Story.query()
+      .withGraphFetched('entries')
+      .withGraphFetched('user(userInfo)')
+      .modifiers({
+        userInfo(builder) {
+          builder.select('id', 'username')
+        },
+      })
+      .findById(id)
+
+    res.status(200).json(story)
+  } catch (error) {
+    next(error)
+  }
+})
+
+//Create a new story
 
 router.post('/create', async (req, res, next) => {
   const { title } = req.body
   try {
-    await schema.validate(title)
+    await schema.validate({ title })
     const decodedToken = await jwt.verify(req.token)
-    if (!req.token || !decodedToken.id) {
+    if (!req.token) {
       const error = new Error('token missing or invalid')
       res.status(401)
       throw error
     }
-    const newStory = Story.query().insert({ title })
+    const newStory = await Story.query().insert({
+      title,
+      author_id: decodedToken.id,
+    })
     res.status(201).json({ msg: `${title} created!` })
   } catch (error) {
     next(error)
   }
 })
+
+router.put('/edit', async () => {})
 
 module.exports = router
