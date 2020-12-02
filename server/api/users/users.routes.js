32a6aafa39d35de1f7a22b9987c3a779b28f1e3d
hector4213 <hectorpwns@gmail.com>
2020-12-02 13:21:27 -0500

    add errors

--- server/api/users/users.routes.js
+++ server/api/users/users.routes.js
@@ -3,10 +3,49 @@ const User = require('./users.model')
 
 const router = express.Router()
 
-/* GET users listing. */
+// GET all users
 router.get('/', async (req, res, next) => {
-  const users = await User.query().select('id', 'username', 'email')
-  res.status(200).json(users)
+  const users = await User.query()
+    .select('id', 'username')
+    .withGraphFetched('stories.entries.[user(nameAndId), format(onlyType)]')
+    .modifiers({
+      nameAndId(builder) {
+        builder.select('id', 'username')
+      },
+      onlyType(builder) {
+        builder.select('type')
+      },
+    })
+
+  try {
+    if (users) {
+      return res.status(200).json(users)
+    }
+    return res.status(200).json({ error: 'No users found' })
+  } catch (error) {}
 })
 
+// GET ONE user
+router.get('/:id', async (req, res, next) => {
+  const { id } = req.params
+  const user = await User.query()
+    .select('id', 'username')
+    .withGraphFetched('stories.entries.[user(nameAndId), format(onlyType)]')
+    .modifiers({
+      nameAndId(builder) {
+        builder.select('id', 'username')
+      },
+      onlyType(builder) {
+        builder.select('type')
+      },
+    })
+    .where({ id })
+  try {
+    if (user) {
+      res.status(200).json(user)
+    }
+    return res.status(404).json({ error: 'no user found' })
+  } catch (error) {}
+  return res.json(user)
+})
 module.exports = router
