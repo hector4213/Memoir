10df91b8460201bde08face210b3dc401279d0a6
hector4213 <hectorpwns@gmail.com>
2020-12-08 13:21:26 -0500

    routes for getting all foreign entries and change entry status

--- server/api/users/manage/manage.routes.js
+++ server/api/users/manage/manage.routes.js
@@ -6,17 +6,62 @@ const jwt = require('../../../lib/jwt')
 const router = express.Router({ mergeParams: true })
 
 router.get('/', async (req, res, next) => {
-  //get array of users story ids
-  const userStories = Story.query()
-    .select('id')
-    .where({ author_id: req.params.user })
-  // Find all entries in all of users stories where user is not entry author
+  const { user } = req.params
+
+  if (!req.token) {
+    res.status(401).json({ error: 'unauthenticated' })
+  }
+  const decodedToken = await jwt.verify(req.token)
+  const isOwner = decodedToken.id === Number(user)
+
+  if (!isOwner) {
+    return res.status(401).json({ error: 'You are not the owner' })
+  }
+  const userStories = Story.query().select('id').where({ author_id: user })
   const foreignEntries = await Entry.query()
     .whereIn('story_id', userStories)
-    .whereNot({ user_id: req.params.user })
-    .withGraphFetched('user')
+    .whereNot({ user_id: user })
+    .withGraphFetched('[user(nameAndId), current(noId)]')
+    .modifiers({
+      nameAndId(builder) {
+        builder.select('id', 'username')
+      },
+      noId(builder) {
+        builder.select('status')
+      },
+    })
 
-  res.sendStatus(200).json(foreignEntries)
+  try {
+    if (foreignEntries) {
+      return res.status(200).json(foreignEntries)
+    }
+    return res.status(200).json({ msg: 'All entries are written by you' })
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.put('/:id', async (req, res, next) => {
+  const { entry_status } = req.body
+  const { id } = req.params
+  if (!req.token) {
+    res.status(401).json({ error: 'unauthenticated' })
+  }
+  const decodedToken = await jwt.verify(req.token)
+  const isOwner = decodedToken.id === Number(user)
+  const updateItem = await Entry.query().findById(id)
+  if (!isOwner) {
+    return res.status(401).json({ error: 'You are not the owner' })
+  }
+
+  try {
+    if (updateItem) {
+      await Entry.query().findById(id).patch({ entry_status })
+      return res.status(200).json({ msg: 'Entry status updated' })
+    }
+  } catch (error) {
+    next(error)
+  }
 })
 
 module.exports = router
