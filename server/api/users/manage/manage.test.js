6a38d9cd08cffc7b712aac9112091bc1eb4305dc
hector4213 <hectorpwns@gmail.com>
2020-12-08 07:23:59 -0500

    test done

--- server/api/users/manage/manage.test.js
+++ server/api/users/manage/manage.test.js
@@ -3,6 +3,7 @@ const supertest = require('supertest')
 const { knex } = require('../../../db/config')
 const app = require('../../../app')
 const Entry = require('../../story/entry/entry.model')
+const Story = require('../../story/story.model')
 
 describe('Test Route for /api/profile/*/manage', async () => {
   beforeEach(async () => {
@@ -15,7 +16,7 @@ describe('Test Route for /api/profile/*/manage', async () => {
     await knex.migrate.rollback()
   })
 
-  it('should fetch all entries that belong to users stories not written by user', async () => {
+  it.only("should fetch an array of entries that belong to user's stories not written by user", async () => {
     const login = await supertest(app)
       .post('/api/auth/login')
       .send({ email: 'tester@test.com', password: 'React!123' }) //returns the login response with user.token
@@ -25,5 +26,32 @@ describe('Test Route for /api/profile/*/manage', async () => {
       .get(`/api/profile/${user.id}/manage`)
       .set('Authorization', `bearer ${token}`)
       .expect(200)
+
+    const userStories = Story.query().select('id').where({ author_id: user.id })
+
+    const allUserEntries = await Entry.query().whereIn('story_id', userStories) // All entries in user stories
+    const onlyUserEntries = await Entry.query().where({ user_id: user.id })
+
+    expect(response.body).to.be.a('array')
+    expect(response.body.length).to.be.equal(
+      allUserEntries.length - onlyUserEntries.length
+    )
+  })
+
+  it('should change status from approved to denied', async () => {
+    const login = await supertest(app)
+      .post('/api/auth/login')
+      .send({ email: 'tester@test.com', password: 'React!123' })
+    const { token, user } = login.body
+    const userEntry = await Entry.query().where({ user_id: user.id }).first()
+
+    const response = await supertest(app)
+      .put(`/api/profile/${user.id}/manage/${userEntry.id}`)
+      .set('Authorization', `bearer ${token}`)
+      .send({ entry_status: 3 })
+      .expect(200)
+
+    const changedEntry = await Entry.query().where({ user_id: user.id }).first()
+    expect(userEntry.entry_status).to.not.equal(changedEntry.entry_status)
   })
 })
