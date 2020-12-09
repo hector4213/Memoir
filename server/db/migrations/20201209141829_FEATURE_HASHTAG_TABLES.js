64572a36074a2093febf9b66611746cc3a5431e3
hector4213 <hectorpwns@gmail.com>
2020-12-09 14:40:10 -0500

    start hastag model

--- server/db/migrations/20201209141829_FEATURE_HASHTAG_TABLES.js
+++ server/db/migrations/20201209141829_FEATURE_HASHTAG_TABLES.js
@@ -2,14 +2,14 @@ const tableNames = require('../../constants/tableNames')
 
 exports.up = async (knex) => {
   await Promise.all([
-    knex.schema.table(tableNames.hashTagsRelations, (table) => {
+    knex.schema.createTable(tableNames.hashTags, (table) => {
+      table.increments()
+      table.string('tagname')
+    }),
+    knex.schema.createTable(tableNames.hashTagsRelations, (table) => {
       table.increments()
       table.integer('entry_id').references('id').inTable(tableNames.entry)
       table.integer('tag_id').references('id').inTable(tableNames.hashTags)
-    }),
-    knex.schema.table(tableNames.hashTags, (table) => {
-      table.increments()
-      table.string('tagname')
     }),
   ])
 }
