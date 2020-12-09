868fa41b6bc462745bc87bf64a81c030994519eb
hector4213 <hectorpwns@gmail.com>
2020-12-09 13:32:54 -0500

    test for user deletion

--- server/api/users/users.test.js
+++ server/api/users/users.test.js
@@ -1,6 +1,7 @@
 const { expect } = require('chai')
 const supertest = require('supertest')
 const { knex } = require('../../db/config')
+const User = require('./users.model')
 const app = require('../../app')
 
 describe('GET profile /api/users', () => {
@@ -44,5 +45,22 @@ describe('GET profile /api/users', () => {
     ])
     expect(stories).to.be.a('array')
     expect(userEntries).to.be.a('array')
+  })
+
+  it('should delete a users account with a status response of 204 No content ', async () => {
+    const login = await supertest(app)
+      .post('/api/auth/login')
+      .send({ email: 'tester@test.com', password: 'React!123' })
+    const { token, user } = login.body
+
+    const allUsers = await User.query().select('id', 'username')
+
+    const response = await supertest(app)
+      .delete(`/api/profile/${user.id}/`)
+      .set('Authorization', `bearer ${token}`)
+      .expect(204)
+
+    const afterDelete = await User.query().select('id', 'username')
+    expect(afterDelete.length).to.equal(allUsers.length - 1)
   })
 })
