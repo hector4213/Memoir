3925d51ad55306f89825a4d31524795e7a8a1f5a
hector4213 <hectorpwns@gmail.com>
2020-12-13 23:41:25 -0500

    add many to many relationship

--- server/api/users/users.model.js
+++ server/api/users/users.model.js
@@ -9,6 +9,7 @@ class User extends Model {
   static get relationMappings() {
     const Story = require('../story/story.model')
     const Entry = require('../story/entry/entry.model')
+    const StoryLikes = require('../story/storylikes.model')
     return {
       stories: {
         relation: Model.HasManyRelation,
@@ -24,6 +25,19 @@ class User extends Model {
         join: {
           from: 'users.id',
           to: 'entry.user_id',
+        },
+      },
+      inspires: {
+        relation: Model.ManyToManyRelation,
+        modelClass: Story,
+        join: {
+          from: 'users.id',
+          through: {
+            from: 'inspires.user_id',
+            to: 'inspires.story_id',
+            extra: ['inspiring'],
+          },
+          to: 'story.id',
         },
       },
     }
