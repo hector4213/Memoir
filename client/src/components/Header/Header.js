70e0f674f1b238821759ee689568af5e34de6183
intrvertmichael <michaelpaguay@gmail.com>
2020-12-21 08:36:12 -0500

    improved header logo

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -2,25 +2,26 @@ import React, {useRef, useState} from 'react'
 import './Header.scss'
 
 const Header = props => {
-    const [topPos, setTopPos] = useState()
-    const [leftPos, setLeftPos] = useState()
-    const [scaleAmount, setScaleAmount] = useState()
+    const [logoStyle, setLogoStyle] = useState()
     const logo = useRef(null)
 
     const followMouse = e => {
-        setTopPos(`${e.clientY-50}px`) //e.pageY
-        setLeftPos(`${e.clientX - 200}px`) //e.pageX
+        setLogoStyle({
+            top:`${e.clientY}px`,
+            left:`${e.clientX}px`,
+            transform: `scale(0.8)`,
+            position:'fixed'
+        })
     }
 
     const mouseOut = () =>{
-        setTopPos('50%')
-        setLeftPos('50%')
+        setLogoStyle({
+            top: '50%',
+            left: '50%',
+            transform: `scale(1.5)`,
+            position:'absolute'
+        })
     }
-
-    const grow = () => setScaleAmount(1.5)
-    const finishGrow = () => setScaleAmount(1)
-
-    const logoStyle = {top:topPos, left:leftPos, transform: `scale(${scaleAmount})` }
 
 return(
     <div className='header'>
@@ -31,8 +32,8 @@ return(
         >
             <h1>Memoir</h1>
             <p
-                onMouseMove={grow}
-                onMouseOut={finishGrow}
+                // onMouseMove={grow}
+                // onMouseOut={finishGrow}
             >
                 Discover someone’s story. <br/>
                 Share yours.
